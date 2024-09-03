9a420f90a798a0350dc39e6ba8ca7ec4f111bd67
Svein Olav Mork <sveinomork@gmail.com>
2024-09-03 10:12:05 +0200

    third

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,4 @@ from prehooks.main import add
 
 
 def test_add():
-    assert add(2, 3) == 5  # This should pass if add is implemented correctly
+    assert add(2, 3) == 6  # This should pass if add is implemented correctly
